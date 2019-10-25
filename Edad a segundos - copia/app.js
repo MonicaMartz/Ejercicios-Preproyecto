ec77492473a5f5f12ae1325f5623fbeb55b4d130
MonicaMartz <monik92_ms@hotmail.com>
2019-10-24 21:54:39 -0500

    copia de edad

---
+++ Edad a segundos - copia/app.js
@@ -0,0 +1,5 @@
+var age = prompt("¿Cuál es tu edad?")
+//convirtiendo edad a segundos
+var ageInSeconds = age*365*24*60*60
+//mostrando edad en la web
+document.write("Tu edad es "+ age+ " y en segundos sería " + ageInSeconds)
